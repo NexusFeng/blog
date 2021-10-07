02a06e74276f78cb0d058d9c4b82c7d1576fd913
NexusFeng <13110025051@163.com>
2021-10-07 20:08:09 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  title: 'NexusFeng的博客',
+  description: '前端学习日记',
+  themeConfig:{
+    nav: [{text: "主页", link: "/"      },
+        { text: "node", link: "/node/" },
+        { text: "前端", link: "/webframe/"},
+        { text: "数据库", link: "/database/"   },
+        { text: "android", link: "/android/"   },
+        { text: "面试问题", link: "/interview/" }
+      ],
+  },
+  themeConfig:{
+    sidebar: 'auto',
+    // sidebarDepth: 1 
+  }
+}
+
