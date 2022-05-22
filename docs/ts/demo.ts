a7e897f1376a052e48ba3f89e429747bf870cc40
NexusFeng <1622920569@qq.com>
2022-05-22 22:32:59 +0800

    interview

--- docs/ts/demo.ts
+++ docs/ts/demo.ts
@@ -1,2 +1,28 @@
 // 注释
-const students: string = 'feng'
+function binarySearch1(arr: number[], target: number): number {
+  const length = arr.length
+  if (length === 0) return -1
+
+  let startIndex = 0 // 开始位置
+  let endIndex = length - 1 // 结束位置
+
+  while(startIndex <= endIndex) {
+    const midIndex = Math.floor((startIndex + endIndex)/2)
+    const midVal = arr[midIndex]
+    if(target < midVal) {
+      // 目标值较小,继续在左侧查找
+      endIndex = midIndex - 1
+    } else if(target > midVal) {
+      // 目标值较大,继续在右侧查找
+      startIndex = midIndex + 1
+    } else {
+      return midIndex
+    }
+  }
+
+  return -1
+}
+
+const arr = [10,20,30,40,50,60,70]
+const target = 20
+console.log(binarySearch1(arr,target))
