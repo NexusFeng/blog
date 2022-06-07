ab6158320f4730c8ccfea9670ffc6fefd16cf195
NexusFeng <1622920569@qq.com>
2022-06-07 10:37:48 +0800

    二叉树

--- test.js
+++ test.js
@@ -1,25 +1,36 @@
-let lengthOfLIS = function(nums) {
-  let dp = []
-  let n = nums.length
+let lengthOfLIS = function (nums) {
+  let dp = [];
+  let n = nums.length;
   if (!n) {
-    return 0
+    return 0;
   }
 
-  dp[0] = 1
+  dp[0] = 1;
   for (let i = 1; i < n; i++) {
-    let num = nums[i]
-    let max = 1
+    let num = nums[i];
+    let max = 1;
     // j从[0, j)依次求出可以和i组成的最长上升子序列
     for (let j = 0; j < i; j++) {
-      let prevNum = nums[j]
+      let prevNum = nums[j];
       if (num > prevNum) {
         // 循环中不断更新max值
-        max = Math.max(max, dp[j] + 1)
+        max = Math.max(max, dp[j] + 1);
       }
     }
-    dp[i] = max
+    dp[i] = max;
   }
-  console.log(dp)
-  return Math.max(...dp)
+  console.log(dp);
+  return Math.max(...dp);
+};
+lengthOfLIS([0, 2, 0, 3, 1, 3]);
+
+function changeArg(x) {
+  x = 200;
 }
-lengthOfLIS([0, 2, 0, 3, 1, 3])
+
+let num = 200;
+changeArg(num);
+console.log('changeA', num);
+let obj = { name: 's' };
+changeArg(obj);
+console.log('obj', obj);
