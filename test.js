9f956fba3eab2846a9e4c50ca460ba95a71856f7
NexusFeng <1622920569@qq.com>
2022-06-20 10:24:18 +0800

    interview

--- test.js
+++ test.js
@@ -1,49 +1,17 @@
-let generateParenthesis = function (n) {
-  let dp = [];
-  dp[0] = [''];
-  dp[1] = ['()'];
-
-  for (let i = 2; i <= n; i++) {
-    let res = [];
-    for (let j = 0; j <= i - 1; j++) {
-      let inners = dp[j];
-      let outers = dp[i - 1 - j];
-
-      for (let inner of inners) {
-        for (let outer of outers) {
-          res.push(`(${inner})${outer}`);
-        }
-      }
-    }
-    dp[i] = res;
+function changeArg(x) {
+  if (typeof x === 'object') {
+    x.name = 'a';
+  } else {
+    x = 200;
   }
-  return dp[n];
-};
-console.log(generateParenthesis(3));
-
-var a = 'a';
-
-function foo() {
-  console.log(a);
 }
 
-function fn() {
-  var a = 'b';
-  foo.call(this);
-}
-fn();
-
-function foo() {
-  console.log(n);
-  var n = 200;
-  console.log(n);
-}
-var n = 100;
-foo();
-
-function foo() {
-  console.log(a);
-  return;
-  var a = 100;
-}
-foo();
+let num = 100;
+changeArg(num);
+console.log('changeA', num);
+let obj = { name: 's' };
+changeArg(obj);
+console.log('obj', obj);
+let obj1 = { city: 's' };
+changeArg(obj1);
+console.log('obj1', obj1);
