774508de17804711981ff4107338f5b8a9ad7e8a
NexusFeng <1622920569@qq.com>
2022-07-18 10:59:24 +0800

    设计模式&算法题解

--- test.js
+++ test.js
@@ -1,28 +1,6 @@
-var nextGreaterElement = function(n) {
-  const nums = [...('' + n)]
-  let i = nums.length - 2
-  while(i >= 0 && nums[i] >= nums[i + 1]){
-    i--
-  }
-  if(i < 0) {
-    return -1
-  }
-
-  let j = nums.length - 1
-  while(j >= 0 && nums[i] >= nums[j]){
-    j--
-  }
-  [nums[i], nums[j]] = [nums[j], nums[i]]
-  reverse(nums, i + 1)
-  const ans = 0 + nums.join('')
-  return ans > MAX? -1: ans
-};
-const reverse = (nums, begin) => {
-  let i = begin, j = nums.length - 1
-  while(i < j){
-    [nums[i], nums[j]] = [nums[j], nums[i]]
-    i++
-    j--
+let arr = [1,2,3,4,5,6,7]
+for(let i = 0; i < arr.length; i++) {
+  if(arr[i] % 2 ==0) {
+    arr.splice(i, 1)
   }
 }
-nextGreaterElement(12443322)
