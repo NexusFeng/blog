4638c4527622e5d79723896f1aae4c0b7eec427e
NexusFeng <1622920569@qq.com>
2022-07-20 10:50:07 +0800

    算法题解

--- test.js
+++ test.js
@@ -1,14 +1,24 @@
-var numSubarrayProductLessThanK = function(nums, k) {
-  let n = nums.length, ret = 0
-  let prod = 1, i = 0
-  for(let j = 0; j < n; j++) {
-    prod *= nums[j]
-    while(i <= j && prod >= k){
-      prod /= nums[i]
-      i++
-    }
-    ret += j - i + 1
+var findAnagrams = function(s, p) {
+  const sLen = s.length, pLen = p.length
+  if(sLen < pLen) return []
+  const ans = []
+  const sCount = new Array(26).fill(0)
+  const pCount = new Array(26).fill(0)
+  for(let i = 0; i < pLen; i++) {
+    sCount[s[i].charCodeAt() - 'a'.charCodeAt()]++
+    pCount[p[i].charCodeAt() - 'a'.charCodeAt()]++
   }
-  return ret
+  if(sCount.toString() === pCount.toString()) {
+    ans.push(0)
+  }
+  for(let i = 0; i < sLen - pLen; i++) {
+    sCount[s[i].charCodeAt() - 'a'.charCodeAt()]--
+    sCount[s[i + pLen].charCodeAt() - 'a'.charCodeAt()]++
+
+    if(sCount.toString() === pCount.toString()) {
+      ans.push(i + 1)
+    }
+  }
+  return ans
 };
-numSubarrayProductLessThanK([1,2,3], 0)
+console.log(findAnagrams('cbaebabacd', 'abc'))
