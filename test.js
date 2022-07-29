14f42b53d94b140ac794b52c573075921ddca2d6
NexusFeng <1622920569@qq.com>
2022-07-29 23:01:06 +0800

    计算属性原理

--- test.js
+++ test.js
@@ -1,15 +1,9 @@
 let obj = {a:1, b:2}
 let a = 22
-Object.defineProperty(obj, 'a',{
+Object.defineProperty(obj, 'b',{
   get(){
     console.log(333)
     return a
   }
 })
-Object.defineProperty(obj, 'a',{
-  get(){
-    console.log(222)
-    return a
-  }
-})
-console.log(obj.a)
+console.log(obj.b)
