c6cdf21e1ccddb87dc90938dd87f252f7aa2f437
NexusFeng <1622920569@qq.com>
2022-08-09 23:04:39 +0800

    1

--- test.js
+++ test.js
@@ -1,6 +1,14 @@
-let map = new Map()
-map.set('a',1)
-map.set('b',-1)
-for(let item of map) {
-  console.log(item[1])
-}
+var isIsomorphic = function(s, t) {
+  const s2t = {}
+  const t2s = {}
+  const len = s.length
+  for(let i = 0; i < len; i++) {
+    const x = s[i],y = t[i]
+    if((s2t[x] && s2t[x] !== y) || (t2s[y] && t2s[y] !==x)) return false
+    s2t[x] = y
+    t2s[y] = x
+  }
+  return true
+};
+
+isIsomorphic('add','sbx')
