6867d96bfca25540f56de102658ca760e7cf2aa7
NexusFeng <1622920569@qq.com>
2022-08-18 22:32:12 +0800

    1

--- test.js
+++ test.js
@@ -1,94 +1,71 @@
-
-// 保存当前需要收集的全局函数
-let activeReactiveFn = null
-class Depend{
-  constructor(){
-    this.reactiveFns = new Set() // 去重
+const tree = [
+  {
+      "label":"节点1",
+      "value":"值1",
+      "level":1,
+      "children":[
+          {
+              "label":"节点1-1",
+              "value":"值1-1",
+              "level":2,
+              "children":[
+                  {
+                      "label":"节点1-1-1",
+                      "value":"1-1-1",
+                      "level":3
+                  },
+                  {
+                      "label":"节点1-1-2",
+                      "value":"1-1-2",
+                      "level":3
+                  }
+              ]
+          }
+      ]
+  },
+  {
+      "label":"节点2",
+      "value":"值2",
+      "level":1,
+      "children":[
+          {
+              "label":"节点2-1",
+              "value":"值2-1",
+              "level":2,
+              "children":[
+                  {
+                      "label":"节点2-1-1",
+                      "value":"2-1-1",
+                      "level":3
+                  }
+              ]
+          }
+      ]
   }
-  
-  // addDepend(reactiveFn){
-  //   this.reactiveFns.add(reactiveFn)
-  // }
+]
 
-  depend() {
-    if(activeReactiveFn) {
-      this.reactiveFns.add(activeReactiveFn)
-    }
-  }
-
-  notify() {
-    this.reactiveFns.forEach(fn => {
-      fn()
-    })
-  }
-}
-
-// 监听对象的属性变量,Proxy(vue3)/Object.defineProperty(vue2)
-function reactive(obj) {
-  Object.keys(obj).forEach(key => {
-    let value
-    Object.defineProperty(obj, key, {
-      get: function() {
-        const depend = getDepend(obj, key)
-        depend.depend()
-        return value
-      },
-      set: function(newVal) {
-        value = newVal
-        const depend = getDepend(obj, key)
-        depend.notify()
+const getAllPath = (tree) => {
+  const paths = [], stack = tree.map(n => ([n, []]));
+  console.log(stack, 'ss')
+  while (stack.length) {
+      const t = stack.pop();
+      console.log(t, 't')
+      t[1].push(t[0]);
+      console.log(t, 't1')
+      if (!t[0].children) {
+          paths.push([...t[1]]);
+          t[1].pop();
+          continue;
       }
-    })
-  })
-  return obj
-}
-
-
-// 封装一个响应式的函数
-// let activeReactiveFn = null
-function watchFn(fn){
-  activeReactiveFn = fn
-  fn()
-  activeReactiveFn = null
-}
-
-// 封装一个获取depend函数
-const targetMap = new WeakMap()
-function getDepend(target, key) {
-  // 根据target对象获取map
-  let map = targetMap.get(target)
-  if(!map) {
-    map = new Map()
-    targetMap.set(target, map)
+      t[0].children.forEach(cn => stack.push([cn, t[1]]))
   }
-
-  // 根据key获取depend对象
-  let depend = map.get(key)
-  if(!depend) {
-    depend = new Depend()
-    map.set(key, depend)
-  }
-  return depend
+  return paths.map(p => ({
+      "oneId": p[0].value,
+      "oneName": p[0].label,
+      "twoId": p[1].value,
+      "twoName": p[1].label,
+      "treeId": p[2].value,
+      "treeName": p[2].label
+  }));
 }
-
-// 对象的响应式
-const obj = {
-  name: 'why', // 每个属性对应一个depend对象
-  age: 18
-}
-// 变为响应式
-const objProxy = reactive(obj)
-
-watchFn(function () {
-  const newName = objProxy.name
-  console.log(objProxy.name, 'name发生了变化')
-})
-
-watchFn(function () {
-  console.log(objProxy.age, 'age 发生了变化')
-})
-
-// 获取age的函数需要只执行一次
-objProxy.age = 19
-
-objProxy.name = 'feng'
+console.log(getAllPath(tree))
