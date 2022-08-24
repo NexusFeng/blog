692762411e05dfd8d79acb62948d8a90f8d9600f
NexusFeng <1622920569@qq.com>
2022-08-24 23:04:01 +0800

    原理

--- test.js
+++ test.js
@@ -1,34 +1,20 @@
-class lazyMan{
-  name = ''
-  stack = []
-  constructor(name){
-    this.name = name
-    setTimeout(() => {
-      this.next()
-    })
+function newSetInterval(fn, t){
+  let timer = null
+  function interval(){
+    fn()
+    timer = setTimeout(interval, t)
   }
-  next(){
-    const task = this.stack.shift()
-    if(task) task()
-  }
-  eat(food){
-    const task = () => {
-      console.info(`${this.name}eat${food}`)
-      this.next()
+  interval()
+  return {
+    cancel: () => {
+      clearTimeout(timer)
     }
-    this.stack.push(task)
-    return this
-  }
-  sleep(second) {
-    const task = () => {
-      setTimeout(() => {
-        console.info(`等待${second}秒,执行下一个任务`)
-        this.next()
-      }, second)
-    }
-    this.stack.push(task)
-    return this
   }
 }
-let me = new lazyMan('nexus')
-me.eat('1').eat('2').sleep(2000).eat('3')
+function a() {
+  console.log(1)
+}
+let time = newSetInterval(a, 2000)
+setTimeout(() => {
+  time.cancel()
+}, 10000)
