f649f6417fc960ab6c3299e267fcf8de44069dbb
NexusFeng <1622920569@qq.com>
2022-09-15 10:18:51 +0800

    udp: 文档

--- test.js
+++ test.js
@@ -1,34 +1,27 @@
-
-function compose(...funcs){
-  //没有放函数，那就返回传入参数
-  if(funcs.length === 0)return (num)=>num
-  //函数组合长度为1，返回函数
-  if(funcs.length === 1)return funcs[0]
-  //其余情况，对funcs进行reduce，里面的函数会从左往右执行，
-  //第一个函数执行完返回的结果将会传递到下一个函数中作为参数继续执行
-  
-  //总体会返回一个方法，用法：compose(函数组合)(传入参数num)
-  return funcs.reduce((pre,cur)=>{
-    //返回一个函数作为下一个pre，函数内执行cur(pre(num)),
-    return (num)=>{
-      //pre(num)是函数组合内上一个函数执行,
-      //cur(pre(num))指的是当前函数执行
-      //返回cur(pre(num))作为新的参数给下个函数继续执行
-      return cur(pre(num))
-    }
-  })
-}
-function fn1(x){
-  return x + 1
-}
-function fn2(x){
-  return x + 2
-}
-function fn3(x){
-  return x + 3
-}
-function fn4(x){
-  return x + 4
-}
-const a = compose(fn1,fn2,fn3,fn4)
-console.log(a(1))// 11 相当于fn1(fn2(fn3(fn4(1))))
+let promise1 = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    resolve()
+  },1000)
+})
+let promise2 = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    resolve()
+  },2000)
+})
+let promise3 = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    resolve()
+  },3000)
+})
+let promise4 = new Promise((resolve,reject) => {
+  setTimeout(() => {
+    reject()
+  },4000)
+})
+let arr = [promise1, promise2, promise3, promise4]
+let arr1 = Promise.all(arr).then(() => {
+  console.log('222')
+}).catch(() => {
+  console.log('333')
+})
+console.log(arr1, 'arr1')
