c387674a63f75c7693e049445595cf36e69f825d
NexusFeng <1622920569@qq.com>
2022-09-15 11:30:18 +0800

    fix: 部署

--- .umirc.ts
+++ .umirc.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'dumi';
-
+const repo = 'blob'
 export default defineConfig({
   title: 'NexusFeng',
   mode: 'site',
   logo: '/images/info.png',
   favicon: '/images/info.png',
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
   locales: [['zh-CN', '中文'], ['en-US', 'English']],
   navs: {
     "zh-CN": [
