a5027b545c0194bf5adb06ce03462778e0d08415
NexusFeng <1622920569@qq.com>
2022-09-29 22:50:07 +0800

    udp: setting

--- test.js
+++ test.js
@@ -1,27 +1,10 @@
-let promise1 = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    resolve()
-  },1000)
-})
-let promise2 = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    resolve()
-  },2000)
-})
-let promise3 = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    resolve()
-  },3000)
-})
-let promise4 = new Promise((resolve,reject) => {
-  setTimeout(() => {
-    reject()
-  },4000)
-})
-let arr = [promise1, promise2, promise3, promise4]
-let arr1 = Promise.all(arr).then(() => {
-  console.log('222')
-}).catch(() => {
-  console.log('333')
-})
-console.log(arr1, 'arr1')
+function num(str) {
+  let arr = str.split(' ')
+  if(arr.length < 2) return 0
+  let set = new Set()
+  for(let i = 0; i < arr.length; i++) {
+    set.add(arr[i].toLowerCase())
+  }
+  return arr.length - set.size
+}
+console.log(num('dog cat DOG'))
