f8645cc51f369865af8f0bd3ebea58926e58ecda
NexusFeng <1622920569@qq.com>
2022-10-08 21:24:23 +0800

    fix: correct

--- test.js
+++ test.js
@@ -1,10 +1,38 @@
-function num(str) {
-  let arr = str.split(' ')
-  if(arr.length < 2) return 0
-  let set = new Set()
-  for(let i = 0; i < arr.length; i++) {
-    set.add(arr[i].toLowerCase())
+function getAllPath (tree) {
+  const paths = []
+  for(let i = 0; i < tree.length; i++) {
+    if(tree[i].children && tree[i].children.length) {
+      const res = getAllPath(tree[i].children)
+      for(let j = 0; j < res.length; j++) {
+        paths.push([tree[i], ...res[j]])
+      }
+    } else {
+      paths.push([tree[i]])
+    }
   }
-  return arr.length - set.size
+  return paths
 }
-console.log(num('dog cat DOG'))
+let tree = [{
+  name: '1',
+  children: [
+    {
+      name: '1-1',
+      children: [
+        {
+          name: '1-1-1',
+          children: []
+        }
+      ]
+    },
+    {
+      name: '1-2',
+      children: [
+        {
+          name: '1-2-1',
+          children: []
+        }
+      ]
+    },
+  ]
+}]
+console.log(getAllPath(tree))
