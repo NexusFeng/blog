b766901cb8c48acad632057e67c11e21cd1bca25
NexusFeng <1622920569@qq.com>
2022-10-15 18:54:37 +0800

    udp: 手写

--- test.js
+++ test.js
@@ -1,38 +1,8 @@
-function getAllPath (tree) {
-  const paths = []
-  for(let i = 0; i < tree.length; i++) {
-    if(tree[i].children && tree[i].children.length) {
-      const res = getAllPath(tree[i].children)
-      for(let j = 0; j < res.length; j++) {
-        paths.push([tree[i], ...res[j]])
-      }
-    } else {
-      paths.push([tree[i]])
-    }
-  }
-  return paths
+let arr = [1,2,[3,[4]]]
+function flatten(arr) {
+  if(!arr.length) return
+  return arr.reduce((pre,cur) => 
+    { return Array.isArray(cur)? [...pre, ...flatten(cur)]: [...pre, cur]},[]
+  )
 }
-let tree = [{
-  name: '1',
-  children: [
-    {
-      name: '1-1',
-      children: [
-        {
-          name: '1-1-1',
-          children: []
-        }
-      ]
-    },
-    {
-      name: '1-2',
-      children: [
-        {
-          name: '1-2-1',
-          children: []
-        }
-      ]
-    },
-  ]
-}]
-console.log(getAllPath(tree))
+console.log(flatten(arr))
