b4f097ae499fe605e0971e830fe5db2841048741
NexusFeng <1622920569@qq.com>
2022-10-17 22:43:31 +0800

    fix: change error

--- test.js
+++ test.js
@@ -1,8 +1,59 @@
-let arr = [1,2,[3,[4]]]
-function flatten(arr) {
-  if(!arr.length) return
-  return arr.reduce((pre,cur) => 
-    { return Array.isArray(cur)? [...pre, ...flatten(cur)]: [...pre, cur]},[]
-  )
+const bfs = root => {
+  const stacks = [root]
+  while(stacks.length) {
+    const stack = stacks.pop()
+    console.log(stack.val)
+    stack.children.forEach(child => {
+      stacks.push(child)
+    })
+  }
 }
-console.log(flatten(arr))
+
+const dfs = root => {
+  console.log(root.val)
+  root.children.forEach(item => {
+    dfs(item)
+  })
+}
+
+function transTree(data) {
+  let result = []
+  let map = {}
+  if(!Array.isArray(data)) return []
+  data.forEach(item => map[item.id] = item)
+  data.forEach(item => {
+    let parent = map[item.parentId]
+    if(parent) {
+      (parent.children || (parent.children = [])).push(item)
+    } else {
+      result.push(item)
+    }
+  })
+  return result
+}
+
+function transArr(tree){
+  let queue = [node]
+  let data = []
+  while(queue.length){
+    let item = queue.shift()
+    data.push({
+      id: item.id,
+      parentId: item.parentId,
+      name: item.name
+    })
+    let children = item.children
+    if(children) {
+      for(let i = 0; i < children.length; i++) {
+        queue.push(children[i])
+      }
+    }
+  }
+  return data
+}
+
+Object.create = function(o){
+  function fn(){}
+  fn.prototype = o
+  return new fn()
+}
