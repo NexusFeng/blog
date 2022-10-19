d1eec0f547b04da6183d01209642ddaef707ba84
NexusFeng <1622920569@qq.com>
2022-10-19 23:00:01 +0800

    add: 手写

--- test.js
+++ test.js
@@ -1,59 +1,16 @@
-const bfs = root => {
-  const stacks = [root]
-  while(stacks.length) {
-    const stack = stacks.pop()
-    console.log(stack.val)
-    stack.children.forEach(child => {
-      stacks.push(child)
-    })
-  }
-}
-
-const dfs = root => {
-  console.log(root.val)
-  root.children.forEach(item => {
-    dfs(item)
-  })
-}
-
-function transTree(data) {
-  let result = []
-  let map = {}
-  if(!Array.isArray(data)) return []
-  data.forEach(item => map[item.id] = item)
-  data.forEach(item => {
-    let parent = map[item.parentId]
-    if(parent) {
-      (parent.children || (parent.children = [])).push(item)
-    } else {
-      result.push(item)
+function getRepititions(str) {
+  if(!str) return 0
+  let num = 0,set = new Set(),start=0,end = 0
+  while(end < str.length){
+    str[end] === str[start]?set.add(str[end]):start = end
+    end++
+    if(start+1==end && str[end] !== str[start]){
+      num = Math.max(set.size, num)
+      set.clear()
     }
-  })
-  return result
-}
-
-function transArr(tree){
-  let queue = [node]
-  let data = []
-  while(queue.length){
-    let item = queue.shift()
-    data.push({
-      id: item.id,
-      parentId: item.parentId,
-      name: item.name
-    })
-    let children = item.children
-    if(children) {
-      for(let i = 0; i < children.length; i++) {
-        queue.push(children[i])
-      }
-    }
+    
   }
-  return data
+  return num
 }
 
-Object.create = function(o){
-  function fn(){}
-  fn.prototype = o
-  return new fn()
-}
+console.log(getRepititions('a'))
