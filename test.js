e386da52e68bc422b172480eeff6cb74405617e2
NexusFeng <1622920569@qq.com>
2022-11-13 20:47:45 +0800

    udp: change

--- test.js
+++ test.js
@@ -1,43 +1,14 @@
-class Add1 {
-  constructor(){
-    this.tasks = []
-  }
-  add(){
-    this.tasks.push(1)
-  }
-  del() {
-    this.tasks.shift()
-  }
-}
-class Add2 {
-  constructor(){
-    this.tasks = []
-    this.perTask = []
-  }
-  add(){
-    this.tasks.push(1)
-  }
-  del() {
-    while(this.tasks.length) {
-      this.perTask.push(this.tasks.pop())
-    }
-    this.perTask.pop()
-  }
-}
 
-console.time('add1')
-let add1 = new Add1()
-for(let i = 0; i < 10000; i ++) {
-  add1.add()
+const arr = [1,[2,[3]]]
+function flatten1(arr) {
+  const res = []
+  const _falt = arr => {
+    arr.forEach(item => {
+      Array.isArray(item) ? _falt(item) : res.push(item)
+    })
+  }
+  _falt(arr)
+  return res
 }
-add1.del()
-console.timeEnd('add1')
+console.log(flatten1(arr))
 
-
-console.time('add2')
-let add2 = new Add2()
-for(let i = 0; i < 10000; i ++) {
-  add2.add()
-}
-add2.del()
-console.timeEnd('add2')
