8d4e2e15307a1b71143ead30e39ab3fe3fc6a032
NexusFeng <1622920569@qq.com>
2023-02-02 23:00:40 +0800

    udp: '手写七'

--- test.js
+++ test.js
@@ -1,14 +1,28 @@
-
-const F = function () {}
-Object.prototype.a = function () {
-  console.log('a')
+const tree = [
+  { name: 'A' },
+  {
+    name: 'B',
+    children: [{ name: 'A' }, { name: 'AA', children: [{ name: 'AAA' }] }],
+  },
+  { name: 'C' },
+]
+const searchTree = (tree, str) => {
+  if (!tree) return [];//处理叶节点
+  let result = [];
+  tree.map((item) => {
+    let it = { ...item }; //每项浅拷贝一下保证不影响原来的tree
+    if (it.name === str) {
+      result.push(it);
+    } else {
+      let obj = searchTree(it.children, str);
+      //得到children的返回的result数组，放到拷贝对象的children中
+      if (obj.length > 0) {
+        it.children = obj;
+        result.push(it);
+      }
+    }
+  });
+  return result;//每层返回一个数组
 }
 
-Function.prototype.b = function () {
-  console.log('b')
-}
-
-const f = new F()
-
-f.a() // a
-f.b() // b
+console.log(searchTree(tree, 'A'))
