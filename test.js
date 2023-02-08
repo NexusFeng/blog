d0966ff510f7aa6c87bf828ac6e97043bb100d8c
NexusFeng <1622920569@qq.com>
2023-02-08 22:51:21 +0800

    fix: '一些小问题'

--- test.js
+++ test.js
@@ -1,28 +1,128 @@
-const tree = [
-  { name: 'A' },
-  {
-    name: 'B',
-    children: [{ name: 'A' }, { name: 'AA', children: [{ name: 'AAA' }] }],
-  },
-  { name: 'C' },
-]
-const searchTree = (tree, str) => {
-  if (!tree) return [];//处理叶节点
-  let result = [];
-  tree.map((item) => {
-    let it = { ...item }; //每项浅拷贝一下保证不影响原来的tree
-    if (it.name === str) {
-      result.push(it);
+class EventEmitter {
+  constructor(){
+    this.events = {}
+  }
+
+  on(type, cb){
+    if(!this.events) this.events = Object.create(null)
+    if(!this.events[type]) {
+      this.events[type] = [cb]
     } else {
-      let obj = searchTree(it.children, str);
-      //得到children的返回的result数组，放到拷贝对象的children中
-      if (obj.length > 0) {
-        it.children = obj;
-        result.push(it);
-      }
+      this.events[type].push(cb)
     }
-  });
-  return result;//每层返回一个数组
+  }
+
+  off(type, cb) {
+    if(!this.events[type]) return
+    if(cb) {
+      this.events[type] = this.events[type].filter(item => {
+        item != cb
+      })
+    } else {
+      delete this.events[type]
+    }
+  }
+
+  emit(type, ...args) {
+    this.events[type] && this.events[type].forEach(cb => {
+      if(cb) {
+        cb.apply(this, args)
+        cb.isOnce && this.off(type, cb)
+      }
+    })
+  }
+
+  once(type, cb) {
+    cb.isOnce = true
+    this.on(type, cb)
+  }
+
 }
 
-console.log(searchTree(tree, 'A'))
+function parseQueryParams(url){
+  return url.split('?')[1].split('&').reduce((target, cur) => {
+    const [key, val] = cur.split('=')
+    target[key] = val
+    return target
+  }, {})
+}
+
+
+const observer = new IntersectionObserver((entires, self) => {
+  entires.forEach(entry => {
+    if(entry.isIntersecting) {
+      const {target} = entry
+      const src = target.dataset.src
+      target.src = src
+      target.removeAttribute('data-src')
+      self.unobserve(target)
+    }
+  })
+})
+
+imgs.forEach(img => {
+  observer.observe(img)
+})
+
+
+
+class myPromise {
+  static all(promises) {
+    return new Promise((resolve, reject) => {
+      let count = 0
+      const result = []
+      for(let i = 0; i < promises.length; i++) {
+        const cur = promises[i]
+        Promise.resolve(item).then(res => {
+          result[i] = res
+          count++
+          if(count === promises.length)
+            resolve(result)
+        }).catch(err => {
+          reject(err)
+        })
+      }
+    })
+  }
+
+  static allSettled(promises){
+    let count = 0
+    let len = promises.length
+    let result = []
+    return new Promise((resolve, reject) => {
+      if(!Array.isArray(promises)) {
+        return reject(new Error('必须传入数组类型'));
+      }
+      promises.forEach((promise, idx) => {
+        promise.then(res => {
+          result[idx] = {
+            status: 'fulfilled',
+            value: res
+          }
+          count++
+          if(count === len) resolve(result)
+        }).catch(err => {
+          result[idx] = {
+            reason: err,
+            status: 'rejected'
+          }
+          count++
+          if(count === len) resolve(result)
+        })
+      })
+    })
+  }
+
+  static any(promises) {
+    return new Promise((resolve, reject) => {
+      promises.then(resolve, reject)
+    })
+  }
+
+  static race(promises) {
+    const res = []
+    return new Promise((resolve, reject) => {
+      
+    })
+  }
+}
