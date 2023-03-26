e6f08b2642ea3adf0b18cf1edebb63aa316e57df
NexusFeng <1622920569@qq.com>
2023-03-26 19:59:43 +0800

    udp: '1'

--- test.js
+++ test.js
@@ -1,32 +1,25 @@
-Function.prototype._bind = function (context,...outsideArgs) {
-  context = (context !== undefined && context !== null) ? context : window
-  let fn = Symbol()
-  context[fn] = this
-  let outsideThis = this
-  const res = function(...innerArgs) {
-    let innerThis = this
-    if(innerThis instanceof outsideThis) {
-      innerThis[fn] = outsideThis
-      return innerThis[fn](...[...innerArgs, ...outsideArgs])
+function debounce(fn, wait, immediate = false) {
+  let timer = null, isFirstTime = false
+  return function(...args){
+    if(timer) {
+      clearTimeout(timer)
+      timer = null
     }
-    return context[fn](...[...innerArgs, ...outsideArgs])
+    if(immediate) {
+      fn.apply(this, args)
+      isFirstTime = true
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, args)
+      clearTimeout(timer)
+      isFirstTime = false
+      timer = null
+    }, wait)
   }
-  if(res.prototype) res.prototype = Object.create(this.prototype)
-  return res
 }
 
-function Point(x, y) {
-  this.x = x;
-  this.y = y;
+function fn(){
+  console.log('deb')
 }
 
-Point.prototype.toString = function() {
-  return this.x + ',' + this.y;
-};
-
-
-var emptyObj = {};
-var YAxisPoint = Point._bind(emptyObj, 0);
-var axisPoint = new YAxisPoint(5);
-console.log(axisPoint.toString())// 5,0
-
+debounce(fn, 1000, true)()
